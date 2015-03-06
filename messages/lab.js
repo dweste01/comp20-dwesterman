be5b641cf52615e90fdee50f3a8b6cf9e3775b67
Danielle Westerman <daniellepwesterman@gmail.com>
2015-03-05 20:04:27 -0500

    added code from class

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,32 @@
 // Your JavaScript goes here...
+
+function parse(){
+
+	// 1: create instance of object
+	request = new XMLHttpRequeset();
+
+	// 3: set up way to managa response --to a function
+	request.onreadystatechange = parseData;
+
+	// 2: create/open HTTP request
+	request.open("GET", "data.json", true);
+
+	// 4: execute request
+	request.send();
+
+}
+
+
+
+
+function parseData() {
+	if (request.readyState ==4) {
+		messagesDiv = document.getElementById("messages");
+		converted = JSON.parse(requestresponseText);
+		for (i = 0; i < converted.length; i++) {
+			messagesDiv.innerHTML += "<p>" + converted [1]['content'] + "</p>";
+		}
+
+	}
+
+}
