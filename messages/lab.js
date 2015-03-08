1d4b212de8a203327f8bccb3e33c8e310c999386
Danielle Westerman <daniellepwesterman@gmail.com>
2015-03-08 17:59:09 -0400

    fixed reference error

--- messages/lab.js
+++ messages/lab.js
@@ -22,7 +22,7 @@ function parse(){
 function parseData() {
 	if (request.readyState ==4) {
 		messagesDiv = document.getElementById("messages");
-		converted = JSON.parse(requestresponseText);
+		converted = JSON.parse(request.responseText);
 		for (i = 0; i < converted.length; i++) {
 			messagesDiv.innerHTML += "<p>" + converted [1]['content'] + "</p>";
 		}
