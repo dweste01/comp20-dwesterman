a66535bf7abf6515067d68cd74833e18de5ba4c4
Danielle Westerman <daniellepwesterman@gmail.com>
2015-03-12 14:00:38 -0400

    bug fixed, added haversine

--- mmap/map.js
+++ mmap/map.js
@@ -15,7 +15,7 @@ var request = new XMLHttpRequest();
 var me_test = new google.maps.LatLng(my_lat, my_lon);
 var me = new google.maps.LatLng(my_lat, my_lon);
 var myOptions = {
-	zoom: 13,
+	zoom: 15,
 	center: me_test,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
@@ -33,11 +33,9 @@ function init() {
 	map = new google.maps.Map(document.getElementById('map-canvas'),
             myOptions);
 	getMyLocation();
-	console.log("Call after getMyLocation()");
 }
 
 function getMyLocation() {
-	console.log("In getMyLocation()");
 	if (navigator.geolocation) { // if it's supported
 		navigator.geolocation.getCurrentPosition(function(position) {
 			my_lat = position.coords.latitude;
@@ -52,18 +50,12 @@ function getMyLocation() {
 	} else {
 		alert("Geolocation is not supported by this browser.");
 	}
-	console.log("Leaving getMyLocation()");
 }
 
 function renderMap() {
-	console.log("in renderMap()");
-	me = new google.maps.LatLng(my_lat, my_lon);
-	console.log(my_lat);
-	console.log(my_lon);
 
-	console.log ("before panto me");
+	me = new google.maps.LatLng(my_lat, my_lon);
 	map.panTo(me);
-	console.log("after panto me");
 
 	// create marker
     	var img = 'liz.jpg';
@@ -73,20 +65,17 @@ function renderMap() {
         	icon: img
 	});
 
-	console.log("created marker");
-
 	marker.setMap(map);
 
 	// open info window when you click on the marker
 	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.close();
 		infowindow.setContent(marker.title);
 		infowindow.open(map, marker);
 	});
 
-	console.log("done with renderMap()");
-
-    // send location info to datastore API
-    request.onreadystatechange = function(){
+    	// send location info to datastore API
+    	request.onreadystatechange = function(){
         if (request.readyState == 4 && request.status == 200) {
             alert("Got places back!");
             data = JSON.parse(request.responseText);
@@ -104,25 +93,50 @@ function renderMap() {
 }
 
 
+function haversine(lat, lng){
+
+	Number.prototype.toRad = function() {
+		return this * Math.PI / 180;
+	}
+// lat = lat1
+// my_lat = lat2
+	var R = 6371;
+	var x1 = my_lat - lat;
+	var dLat = x1.toRad();
+	var x2 = my_lon - lng;
+	var dLon = x2.toRad();
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+	                Math.cos(lat.toRad()) * Math.cos(my_lat.toRad()) * 
+	                Math.sin(dLon/2) * Math.sin(dLon/2);  
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; 
+	return d;
+
+}
+
 
 function createOtherMarkers(place) {
 
-	console.log(place.login);
-	// console.log(place.lat);
-	// console.log(place.lng);
 	newloc = new google.maps.LatLng(place.lat, place.lng);
+
+	dis = haversine(place.lat, place.lng);
+
 	newmarker = new google.maps.Marker({
 		position: newloc,
+		map: map,
 		title: place.login
 	});
 
-	newmarker.setMap(map);
-
+	content = '<div> <p>' + place.login + " is " + dis + " miles away </p> </div>" 
+	newmarker.content = content;
+	new_iw = new google.maps.InfoWindow();
 	google.maps.event.addListener(newmarker, 'click', function() {
-		infowindow.setContent(newmarker.title);
-		infowindow.open(map, newmarker);
+		new_iw.setContent(this.content);
+		new_iw.open(this.getMap(), this);
+		map.panTo(this.position);
 	});
 
+	newmarker.setMap(map);
 
  // var placeLoc = place.geometry.location;
  // var marker = new google.maps.Marker({
